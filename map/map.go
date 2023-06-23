25e40a23d7e31a9f1560e0dfc92d6380dc23834b
Hossein <hosseinrouhi79@gmail.com>
2023-06-23 22:12:55 +0330

    complete map

---
+++ map/map.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	myMap := make(map[string]string)
+	fmt.Println(myMap)
+
+	myMap2 := map[string]string{
+		"215479962": "Ali",
+		"215479963": "Vahid",
+		"215479964": "Milad",
+		"215479965": "Reza",
+	}
+	fmt.Println(myMap2)
+	myMap2["215479965"] = "Mahtab" //update element
+	fmt.Println(myMap2)
+
+	delete(myMap2, "215479965") //delete element
+	fmt.Println(myMap2)
+}
